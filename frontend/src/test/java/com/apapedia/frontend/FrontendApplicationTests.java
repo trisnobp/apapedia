525c40b11b2555a94c3ff142969aa5a995821ee4
Trisno Bayu Pamungkas <trisno.bayu@ui.ac.id>
2023-11-08 00:02:57 +0700

    feat: initialize spring boot projects and flutter project

---
+++ frontend/src/test/java/com/apapedia/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apapedia.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
