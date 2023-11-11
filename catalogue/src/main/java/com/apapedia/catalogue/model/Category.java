acb60d108d6ebc519c603c511ffd41da742f9d23
Ratu Almas Naurah Anvilen <ratu.almas@ui.ac.id>
2023-11-11 17:05:35 +0700

    fitur cs1

---
+++ catalogue/src/main/java/com/apapedia/catalogue/model/Category.java
@@ -0,0 +1,29 @@
+package com.apapedia.catalogue.model;
+
+import java.util.List;
+import java.util.UUID;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category") 
+public class Category {
+    @Id
+    private UUID id = UUID.randomUUID();
+
+    @Column(name = "nama", nullable = false)
+    private String namaCategory;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Catalogue> catalogs;
+}
---
+++ catalogue/src/main/java/com/apapedia/catalogue/service/CatalogueService.java
@@ -0,0 +1,9 @@
+package com.apapedia.catalogue.service;
+
+import com.apapedia.catalogue.model.Catalogue;
+
+public interface CatalogueService {
+
+    public void createCatalogue(Catalogue catalogue);
+    
+}
