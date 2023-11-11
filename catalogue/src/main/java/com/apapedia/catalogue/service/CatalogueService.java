4aadb2e1e7b62bc52cdd001979579fae7f4255b4
Ratu Almas Naurah Anvilen <ratu.almas@ui.ac.id>
2023-11-11 17:14:06 +0700

    fitur cs3

--- catalogue/src/main/java/com/apapedia/catalogue/service/CatalogueService.java
+++
@@ -1,9 +0,0 @@
-package com.apapedia.catalogue.service;
-
-import com.apapedia.catalogue.model.Catalogue;
-
-public interface CatalogueService {
-
-    public void createCatalogue(Catalogue catalogue);
-    
-}
--- catalogue/src/main/java/com/apapedia/catalogue/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.apapedia.catalogue.service;
-
-import java.util.List;
-
-import com.apapedia.catalogue.model.Category;
-
-public interface CategoryService {
-    public List<Category> getAllCategory();
-}
