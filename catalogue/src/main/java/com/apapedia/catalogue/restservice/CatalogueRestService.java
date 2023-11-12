be1b97555045690762b1138e404286bc1fa7fdd5
Ratu Almas Naurah Anvilen <ratu.almas@ui.ac.id>
2023-11-12 15:03:30 +0700

    fitur cs1

---
+++ catalogue/src/main/java/com/apapedia/catalogue/restservice/CatalogueRestService.java
@@ -0,0 +1,7 @@
+package com.apapedia.catalogue.restservice;
+
+import com.apapedia.catalogue.model.Catalogue;
+
+public interface CatalogueRestService {
+    public void createRestCatalogue(Catalogue catalogue);
+}
