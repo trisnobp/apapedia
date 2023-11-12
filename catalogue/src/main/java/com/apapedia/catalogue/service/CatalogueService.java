f7d7f0a911d5d167b26d4f1b543c22058a8e26ab
Ratu Almas Naurah Anvilen <ratu.almas@ui.ac.id>
2023-11-12 16:00:20 +0700

    fitur cs6

--- catalogue/src/main/java/com/apapedia/catalogue/service/CatalogueService.java
+++ catalogue/src/main/java/com/apapedia/catalogue/service/CatalogueService.java
@@ -1,15 +1,16 @@
 package com.apapedia.catalogue.service;
 
-
 import com.apapedia.catalogue.model.Catalogue;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CatalogueService {
-
     public void createCatalogue(Catalogue catalogue);
 
-    public void deleteCatalogue(Catalogue catalogue);
+    public List<Catalogue> retrieveAllCatalogue();
 
-    public Catalogue getCatalogueById(UUID id);
-    
+    public Optional<Catalogue> getCatalogueById(UUID id);
+
+    public void deleteCatalogue(Catalogue catalogue);
 }
