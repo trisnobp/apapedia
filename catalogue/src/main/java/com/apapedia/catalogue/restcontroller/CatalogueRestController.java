c22b08349c563f53556b0959c7fb13e5e1237bff
Ratu Almas Naurah Anvilen <ratu.almas@ui.ac.id>
2023-11-12 15:08:40 +0700

    fitur cs4

--- catalogue/src/main/java/com/apapedia/catalogue/restcontroller/CatalogueRestController.java
+++ catalogue/src/main/java/com/apapedia/catalogue/restcontroller/CatalogueRestController.java
@@ -7,28 +7,51 @@ import org.hibernate.validator.constraints.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.apapedia.catalogue.DTO.CatalogueMapper;
+import com.apapedia.catalogue.DTO.request.CreateCatalogueRequestDTO;
 import com.apapedia.catalogue.model.Catalogue;
 import com.apapedia.catalogue.restservice.CatalogueRestService;
 
+import jakarta.validation.Valid;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/catalogue")
 public class CatalogueRestController {
 
     @Autowired
     private CatalogueRestService catalogueRestService;
 
-    @GetMapping(value = "/catalogue/view-all")
+    @Autowired
+    private CatalogueMapper catalogueMapper;
+
+    @PostMapping(value = "/create")
+    public Catalogue addCatalogue(@Valid @RequestBody CreateCatalogueRequestDTO catalogueDTO, BindingResult bindingResult){
+        if(bindingResult.hasFieldErrors()){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field"
+                );
+        } else{
+            var catalogue = catalogueMapper.createCatalogueRequestDTOToCatalogue(catalogueDTO);
+            catalogueRestService.createRestCatalogue(catalogue);
+            return catalogue;
+        }
+    }
+
+    @GetMapping(value = "/view-all")
     private List<Catalogue> retrieveAllCatalogue() {
         return catalogueRestService.retrieveRestAllCatalogue();
     }
 
-    @GetMapping("/catalogue/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Catalogue> retrieveCatalogue(@PathVariable UUID id) {
         return catalogueRestService.getRestCatalogueById(id)
             .map(ResponseEntity::ok)
--- catalogue/src/main/java/com/apapedia/catalogue/restservice/CatalogueRestService.java
+++ catalogue/src/main/java/com/apapedia/catalogue/restservice/CatalogueRestService.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface CatalogueRestService {
+    public void createRestCatalogue(Catalogue catalogue);
+
     public List<Catalogue> retrieveRestAllCatalogue();
 
     public Optional<Catalogue> getRestCatalogueById(UUID id);
